0f9698f3a79cace8af487ae803da3b86b01f3e29
sora suke <mrkwsrw@yahoo.co.jp>
2017-08-01 19:44:08 +0900

    1.10.2~1.12で動くようになった

--- java/com/sorasuke/modlistgenerator/ModListGenerator.java
+++ java/com/sorasuke/modlistgenerator/ModListGenerator.java
@@ -26,8 +26,8 @@ public class ModListGenerator {
 
     public static final String MODID = "modlistgenerator";
     public static final String MODNAME = "ModListGenerator";
-    public static final String VERSION = "1.2";
-    public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.10.2,1.11.2]";
+    public static final String VERSION = "1.3";
+    public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.10.2,1.12]";
 
     public static String dirName = "modlist";
     public static String fileName = "list";
